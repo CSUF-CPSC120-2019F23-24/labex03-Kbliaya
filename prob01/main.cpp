3caec97100376a0a3936d4be752601fbebbe1700
Kbliaya <kbliaya@csu.fullerton.edu>
2019-09-07 13:05:41 -0700

    changed values for todo #1, added 3 cout statements for + in todo #2, included escape characters in todo #3, and changed variable and data types for todo #4.

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -6,13 +6,13 @@ int main()
 {
   // TODO#1: try changing these values to others found in the ASCII character set
   // The class Quick Reference Guide contains this chart on page 5
-  char my_char1 = 67;   // decimal
-  char my_char2 = 0x43; // hex
-  char my_char3 = 'C';  // character
+  char my_char1 = 75;   // decimal
+  char my_char2 = 0x4B; // hex
+  char my_char3 = 'K';  // character
 
-  std::cout << "Char1: " << my_char1 << std::endl;
-  std::cout << "Char2: " << my_char2 << std::endl;
-  std::cout << "Char3: " << my_char3 << std::endl;
+  std::cout << "Char1: " << my_char1 << '\n';
+  std::cout << "Char2: " << my_char2 << '\n';
+  std::cout << "Char3: " << my_char3 << '\n';
 
   std::cout << std::endl; // blank line to separate the different exercises
 
@@ -20,14 +20,20 @@ int main()
   // add 3 more cout statements that all print the same thing - a plus sign
   // however, use a different method to do so for each cout statement
   // hint: use the ASCII character set
-  std::cout << "+" << std::endl;
+  char my_char4 = 43; // decimal
+  char my_char5 = 0x2B; // hex
+  char my_char6 = '+'; // character
+  std::cout << "+" << '\n';
+  std::cout<< "Char4: " << my_char4 << '\n';
+  std::cout<< "Char5: " << my_char5 << '\n';
+  std::cout<< "Char6: " << my_char6 << '\n';
 
   std::cout << std::endl; // blank line to separate the different exercises
 
   // TODO#3: update the following statement to include escape characters
   // note that clang does not require an escape character on the single quote
   // but many other compilers do require it
-  std::cout << "'Y'" << std::endl;
+  std::cout << "\'Y\'" << '\n';
 
   std::cout << std::endl; // blank line to separate the different exercises
 
@@ -38,8 +44,8 @@ int main()
   short my_int = 1;
   std::cout << my_int << ' ' << sizeof(my_int) << '\n';
 
-  double amount;
-  std::cout << "A float is stored in " << sizeof(float) << " bytes\n";
+  float amount;
+  std::cout << "A double is stored in " << sizeof(double) << " bytes\n";
   std::cout << "The variable \"amount\" is stored in " << sizeof(amount) << " bytes\n";
 
   return 0;
