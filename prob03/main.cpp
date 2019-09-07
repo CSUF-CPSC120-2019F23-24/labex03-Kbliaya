0454cefde4c8aec21caae3e14fbd65da8e7d9067
Kbliaya <kbliaya@csu.fullerton.edu>
2019-09-07 13:50:55 -0700

    created a program that conversts a person's height in inches to feet and inches.

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,21 @@
+//Name: Kaylee Bliaya
 // This program calculates a person's height in feet (') and inches (").
+#include <iostream>
+
+int main ()
+{
+  int height, feet, inches;
+
+  //Ask the user's height in inches.
+  std::cout<< "Please enter the person's height in inches: ";
+  std::cin>> height;
+
+  //Calculate the person's height in feet and inches.
+  feet = height / 12;
+  inches = height % 12;
+
+  //Display the height in feet and inches.
+  std::cout<< "That person is " << feet << '\'' <<inches<< "\"\n";
+
+  return 0;
+}
