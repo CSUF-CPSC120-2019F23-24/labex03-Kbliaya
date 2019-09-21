8e14af4657757bb047dcb4560c0afec077e35125
kbliaya <kbliaya@cs104u-6e.acad.fullerton.edu>
2019-09-21 14:57:35 -0700

    fixed changes in spacing for problem 3.

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -7,15 +7,15 @@ int main ()
   int height, feet, inches;
 
   //Ask the user's height in inches.
-  std::cout<< "Please enter the person's height in inches: ";
-  std::cin>> height;
+  std::cout << "Please enter the person's height in inches: ";
+  std::cin >> height;
 
   //Calculate the person's height in feet and inches.
   feet = height / 12;
   inches = height % 12;
 
   //Display the height in feet and inches.
-  std::cout<< "That person is " << feet << '\'' <<inches<< "\"\n";
+  std::cout << "That person is " << feet << '\'' << inches << "\"\n";
 
   return 0;
 }
